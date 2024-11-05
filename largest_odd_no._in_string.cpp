e8c39043055c8ea5ac540e04f32c6c0e8477de66
Palak-Patodi <palakpatodi06@gmail.com>
2024-11-05 10:23:25 +0530

    Create largest_odd_no._in_string.cpp

---
+++ largest_odd_no._in_string.cpp
@@ -0,0 +1,13 @@
+//time complexity:O(n)
+//space complexity:O(1)
+class Solution {
+public:
+    string largestOddNumber(string num) {
+         for (int i = num.size() - 1; i >= 0; --i) {
+            if ((num[i] - '0') % 2 == 1) {
+                return num.substr(0, i + 1);
+            }
+        }
+        return "";
+    }
+};
